85492a6c953f0699c100e248fb3edb33363a42de
AlmuUve <avelazqc@gmail.com>
2021-01-25 19:09:22 +0000

    adding footer

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const footerStyle = {
+	background: "#006E90",
+	height: "5rem",
+	display: "flex",
+	justifyContent: "center",
+	alignContent: "center"
+};
+const textStyle = {
+	color: "white",
+	alignSelf: "center",
+	marginTop: "1rem"
+};
+
+const heartStyle = {
+	color: "white",
+	alignSelf: "center",
+	marginLeft: "1rem"
+};
+
+export const Footer = props => {
+	return (
+		<div style={footerStyle}>
+			<p style={textStyle}>{props.text}</p>
+			<i style={heartStyle} className="far fa-heart" />
+		</div>
+	);
+};
+
+Footer.propTypes = {
+	text: PropTypes.string
+};
