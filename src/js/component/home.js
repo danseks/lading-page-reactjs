0f07af9258b4c84c38f8d3fe939c3cc3875471b0
AlmuUve <avelazqc@gmail.com>
2021-01-25 15:55:53 +0000

    adding jumbotron

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -8,7 +8,7 @@ export function Home() {
 	return (
 		<MyJumbotron
 			title="Most expected series in 2021"
-			description="React is the most popular rendering library in the world"
+			description="The TV shows do not stop in 2021, this year comes loaded with news, but also with old acquaintances who return to continue where they left off. Platforms such as Netflix, HBO, Amazon Prime Video, Disney +, Apple TV +, Movistar +, Filmin or Starzplay will bring us from January those that will become the best series of 2021"
 			buttonLabel="Find out all of them!"
 		/>
 	);
